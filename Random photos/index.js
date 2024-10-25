0ef6616f87837b9bf4bccbdf1292ca5e7f79014b
zayn-121 <zayedanwar121@gmail.com>
2024-10-25 19:30:25 +0530

    5th project

---
+++ Random photos/index.js
@@ -0,0 +1,17 @@
+const imageContainer = document.querySelector(".image-container");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+    console.log("clicked");
+    addNewImages();
+})
+let imageNum = 10;
+
+function addNewImages(){
+    for(let i = 0; i < imageNum; i++){
+        const image = document.createElement("img");
+    image.src = `https://picsum.photos/300/300?random=${Math.floor(Math.random() * 2000)}`;
+    imageContainer.appendChild(image);
+    }
+    
+}
